0f4d18305b9dfdc440e1da3e120b04f327188df7
jamesbond002 <jamesbond00700@yandex.com>
2015-04-15 14:45:29 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,14 @@
+{
+"moduleId" : "jamesbond002/osm",
+"title" : "OSM",
+"subtitle" : "www.openstreetmap.org",
+"backButton" : true,
+"enableGPS" : true,
+"zoomControl" : false,
+"screenLockRot" : false,
+"reload" : false,
+"shouldOverrideUrlLoading1" : "http://www.openstreetmap.org",
+"shouldOverrideUrlLoading2" : "https://www.openstreetmap.org",
+"loadUrl" : "http://www.openstreetmap.org/#map=#zoom#/#lat#/#lng#",
+"browserLaunchLink" : "http://www.openstreetmap.org/#map=#zoom#/#lat#/#lng#"
+}
